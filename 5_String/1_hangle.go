ee740c39550b26ad13a33ed40caedd917e3d0059
Gunho Park <devgun@kakao.com>
2021-01-06 19:24:06 +0900

    hangle print

--- 5_String/1_hangle.go
+++ 5_String/1_hangle.go
@@ -7,5 +7,10 @@ func main() {
 		// Type : i-int(32bits), r-rune(32bits)
 		fmt.Println(i, r)
 	}
-	fmt.Println(len("가나다")) // 9 bytes
+
+	fmt.Println("len(가나다) :", len("가나다")) // 9 bytes
+
+	for _, r := range "라마바" { // 3 bytes(24bits) per character is required.
+		fmt.Println(string(r), r)
+	}
 }
