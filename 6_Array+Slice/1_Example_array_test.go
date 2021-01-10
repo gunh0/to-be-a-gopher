e876d77702c0e869623955867eb1a0efb8af3697
Gunho Park <devgun@kakao.com>
2021-01-10 19:38:04 +0900

    Add Array Example

---
+++ 6_Array+Slice/1_Example_array_test.go
@@ -0,0 +1,12 @@
+// go test .\1_Example_array.go
+package main
+
+import "fmt"
+
+func Example_array() {
+	fruits := [4]string{"Apple", "Banana", "Tomato", "Avocado"}
+	for _, fruit := range fruits {
+		fmt.Printf("%s is delicious.\n", fruit)
+	}
+	// Output:
+}
